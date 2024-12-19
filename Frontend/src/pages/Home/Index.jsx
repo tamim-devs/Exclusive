265543b21eafcc14601dca8802e6abc7ff4f457a
tamim-devs <tamimh0727@gmail.com>
2024-12-19 22:41:51 +0600

    world

--- Frontend/src/pages/Home/Index.jsx
+++ Frontend/src/pages/Home/Index.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from '../../components/RootLayout/header/Index'
-import Navbar from '../../components/RootLayout/navbar/Navbar'
-import Banner from '../../components/homePageComponents/banner/Banner'
-import FlashSale from '../../commonComponents/flashsale/FlashSale'
-import Category from '../../components/homePageComponents/category/category'
+import React from 'react';
+import Header from '../../components/RootLayout/header/Index';
+import Navbar from '../../components/RootLayout/navbar/Navbar';
+import Banner from '../../components/homePageComponents/banner/Banner';
+import FlashSale from '../../commonComponents/flashsale/FlashSale';
+import Category from '../../components/homePageComponents/category/category';
 
 const Homepage = () => {
   return (
     <>
-      <Header/>
-      <Navbar/>
-      <Banner/>  
-      <FlashSale/>    
-      <Category/>
+      <Header />
+      <Navbar />
+      <Banner />
+      <FlashSale />
+      <Category />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
